7f94812327e56c47abdaa9b3ed6a0980b8ae1b25
Bjorn Peters <bjornpeters@outlook.com>
2022-04-26 12:57:12 +0200

    Commit last changes for final release

--- Azure Deployment/build.ps1
+++ Azure Deployment/build.ps1
@@ -1,47 +1,101 @@
+# Import the functions
+Import-Module '/Users/bjorn/github/powershell/Azure Deployment/functions.psm1'
+
+Connect-AzAccount
+
+# Stop displaying warning messages from the Az module
+Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
+
 # Credentials
 $userName = "newuser"
 $password = ConvertTo-SecureString "Hello1234" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($userName, $password)
 
-$resourceGroupName = 'TestLab'
-$locationName = 'westeurope'
+# Import JSON configuration file 
+$jsonPrompt = Read-Host -Prompt 'Enter the full file path of the configuration file'
+try {
+    $json = Get-Content $jsonPrompt | ConvertFrom-Json
+}
+catch {
+    Write-Host "Configuration file could not be found. Please enter the correct full path"
+    Exit
+}
 
-$vmName = 'DC01'
-$vmNicName = $vmName + '-nic'
-$vmPublicIP = $vmName + '-ip'
-$vmSize = 'Standard_B2s'
-$publisherName = 'MicrosoftWindowsServer'
-$offer = 'WindowsServer'
-$skus = '2019-datacenter-gensecond'
+# Get the public IP Address from where the script is run
+$remoteAddress = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
 
-$networkName = 'TestLab'
-$vnetAddressPrefix = '10.10.0.0/24'
-$subnetName = 'TestLab'
-$subnetAddressPrefix = '10.10.0.0/24'
+# Check if the resource group is already present. If not, create a new resource group.
+try {
+    Get-AzResourceGroup -Name $json.resourceGroupName -ErrorAction Stop
+    Write-Host "[WARNING] - Resource group already exists ($($json.resourceGroupName))"
+}
+catch {
+    New-AzResourceGroup -Name $json.resourceGroupName -Location $json.locationName | Out-Null
+    Write-Host "[SUCCESS] - Resource group is created ($($json.resourceGroupName))"
+}
 
-# Create a new resource group in Microsoft Azure
-New-AzResourceGroup -Name $resourceGroupName -Location $locationName
+# Create a new subnet and Azure Virtual Network when it does not exist
+$vnet = Get-AzVirtualNetwork -Name $json.vnetName
+if ($vnet) {
+    Write-Host "[WARNING] - Virtual network already exists ($($json.vnetName))"
+}
 
-# Create a public IP address for the VM in Microsoft Azure
-$pip = New-AzPublicIpAddress -Name $VMPublicIP -ResourceGroupName $ResourceGroupName -Location $LocationName -AllocationMethod Dynamic
+else {
+    # Process each subnet that has been defined in the JSON file and add it to the config for the new virtual network
+    foreach ($subnet in $json.subnets) {
+        $subnetConfig = New-AzVirtualNetworkSubnetConfig -Name $subnet.subnetName -AddressPrefix $subnet.subnetPrefix
+        Write-Output "[INFO] - Subnet is added to the virtual network configuration ($($subnet.subnetName))"
+    }
 
-# Create a new virtual network in Microsoft Azure
-$subnet = New-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
-$vnet = New-AzVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $LocationName -AddressPrefix $VnetAddressPrefix -Subnet $Subnet
-$nic = New-AzNetworkInterface -Name $VMNicName -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
+    # Create a new virtual network and include the configured subnets from the previous cmdlet
+    $vnet = New-AzVirtualNetwork -Name $json.vnetName -ResourceGroupName $json.resourceGroupName -Location $json.locationName -AddressPrefix $json.vnetAddressPrefix -Subnet $subnetConfig
+    Write-Host "[SUCCES] - Virtual network is created ($($json.vnetName))"
+}
 
-# Create a new virtual machine in Microsoft Azure
-New-TestLabVM -Credential $credential `
-    -VMName $vmName `
-    -VMSize $vmSize `
-    -NICID $nic.Id `
-    -Skus $skus `
-    -Offer $offer `
-    -ResourceGroupName $resourceGroupName `
-    -LocationName $locationName
+# Process every machine that is found in the JSON file to speed up the virtual machine deployment
+foreach ($machine in $json.machines) {
 
-# Add a new script extension to the VM
-$fileUri = "https://storageaccountbpbjoac05.blob.core.windows.net/scripts/Add-ScriptExtension.ps1"
-$script = 'Add-ScriptExtension.ps1'
+    $vmPublicIP = $machine.vmName + '-ip'
+    $vmNicName = $machine.vmName + '-nic'
 
-Add-ScriptExtension -FileUri $fileUri -Script $script -ResourceGroupName $resourceGroupName -VMName $vmName -LocationName $locationName
+    $pip = New-AzPublicIpAddress -Name $vmPublicIP -ResourceGroupName $json.resourceGroupName -Location $json.locationName -AllocationMethod Dynamic
+    $nic = New-AzNetworkInterface -Name $vmNicName -ResourceGroupName $json.resourceGroupName -Location $json.locationName -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id
+
+    # Create a public IP address, a network interface card and a Network Security Group
+    switch ($machine.type) {
+        "DomainController" { $nsg = New-TestLabNSG -LocationName $json.locationName -RemoteAddress $remoteAddress -Type 'DomainController' }
+        "ExchangeServer" { $nsg = New-TestLabNSG -LocationName $json.locationName -RemoteAddress $remoteAddress -Type 'ExchangeServer' }
+        "Client" { $nsg = New-TestLabNSG -LocationName $json.locationName -RemoteAddress $remoteAddress -Type 'Client' }
+        Default {}
+    }
+
+    # Assign Network Security Group to the network interface card
+    $nic.NetworkSecurityGroup = $nsg
+    $nic | Set-AzNetworkInterface
+
+    # Create a new virtual machine in Microsoft Azure
+    New-TestLabVM -Credential $credential -VMName $machine.vmName -VMSize $machine.vmSize -NICID $nic.Id -Skus $machine.skus -Offer $machine.offer -ResourceGroupName $json.resourceGroupName -LocationName $json.locationName -PublisherName $machine.publisherName
+}
+
+foreach ($machine in $json.machines) {
+    # Based on the type of the virtual machine, execute a different script extension
+    switch ($machine.type) {
+        "DomainController" { 
+            # Add a new script extension to the VM
+            $fileUri = "https://storageaccountbpbjoac05.blob.core.windows.net/scripts/Set-PowerShellRemoting.ps1"
+            $script = 'Set-PowerShellRemoting.ps1'
+
+            Add-ScriptExtension -FileUri $fileUri -Script $script -ResourceGroupName $json.resourceGroupName -VMName $machine.vmName -LocationName $json.locationName
+        }
+        "ExchangeServer" { 
+            # Add a new script extension to the VM
+            $fileUri = "https://storageaccountbpbjoac05.blob.core.windows.net/scripts/Set-PowerShellRemoting.ps1"
+            $script = 'Set-PowerShellRemoting.ps1'
+
+            Add-ScriptExtension -FileUri $fileUri -Script $script -ResourceGroupName $json.resourceGroupName -VMName $machine.vmName -LocationName $json.locationName
+         }
+        "Client" {  }
+        Default {}
+    }
+}
+
